26f8ca23985c8cde41a75b039711c1703ae84ec5
Thyfwx <131562370+Thyfwx@users.noreply.github.com>
2026-04-10 12:43:34 -0500

    Initial Commit

---
+++ Media Organizer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Media Organizer
+//
+//  Created by Xavier Scott on 4/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Media Organizer/Media_OrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Media_OrganizerApp.swift
+//  Media Organizer
+//
+//  Created by Xavier Scott on 4/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Media_OrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
